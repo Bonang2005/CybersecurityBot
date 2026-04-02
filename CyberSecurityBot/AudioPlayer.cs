233b69ffe4433625d6b33682d5013426e6066986
Bonang2005 <mahlakobonang@gmail.com>
2026-04-03 01:42:53 +0200

    Added speech synthesizer in audio player

--- CyberSecurityBot/AudioPlayer.cs
+++ CyberSecurityBot/AudioPlayer.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static System.Console;
+using System.Speech.Synthesis;
 namespace CyberSecurityBot
 {
-    internal class AudioPlayer
+    public static void AudioPlayer
+
     {
+public static void playgreeting()
+    {
+		try
+
+     {
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+            // for setting the volume
+            synthesizer.Volume = 100;
+            // The welcoming voice over
+		
+    }
     }
 }
