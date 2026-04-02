4117a20b76346c36da23e49af22b0488e4ffa35a
Bonang2005 <mahlakobonang@gmail.com>
2026-04-03 00:08:24 +0200

    added a code for the typing effect

--- CyberSecurityBot/Display.cs
+++ CyberSecurityBot/Display.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
@@ -17,6 +18,38 @@ namespace CyberSecurityBot
             Console.WriteLine("  ____                   ____      _               ____        _   \r\n | __ )  ___  ___       / ___|   _| |__   ___ _ __| __ )  ___ | |_ \r\n |  _ \\ / _ \\/ _ \\_____| |  | | | | '_ \\ / _ \\ '__|  _ \\ / _ \\| __|\r\n | |_) |  __/  __/_____| |__| |_| | |_) |  __/ |  | |_) | (_) | |_ \r\n |____/ \\___|\\___|      \\____\\__, |_.__/ \\___|_|  |____/ \\___/ \\__|\r\n                             |___/                                 ");
             Console.WriteLine("Keeping you safe online");
             Console.WriteLine("CyberSecurity Awareness Bot");
+            Console.ResetColor();
+
+        }
+        public static void ShowWelcome(string name)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" =================================================");
+            Console.WriteLine(" Welcome," + name + "!");
+            Console.WriteLine(" I ma your Cybersecurity Awareness Bot.");
+            Console.WriteLine(" Type 'help' to see what i can help you with,");
+            Console.WriteLine(" Type 'exit' to exit");
+            Console.WriteLine(" =================================================");
+            Console.ResetColor();
+
+        }
+        public static void Chat(string message, ConsoleColor colour = ConsoleColor.White)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine(" [CyberBot]" );
+            Console.ForegroundColor = colour; 
+            foreach (char c in massage)// for the typing effect
+            {
+                Console.Write(c);
+                Thread.Sleep(15);// is the milliseconf between each charecter its a pause between each
+
+            }
+            Console.WriteLine();
+            Console.ResetColor();
+
+        }
+        //
         }
     }
-}
+
