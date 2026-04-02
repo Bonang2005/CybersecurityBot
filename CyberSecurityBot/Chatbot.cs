5a3c9b8ba4c99b7d138b3ff0c09e4dfe6880f019
Bonang2005 <mahlakobonang@gmail.com>
2026-04-03 00:48:47 +0200

    In the chatbot class created a userprofile to track the session information

--- CyberSecurityBot/Chatbot.cs
+++ CyberSecurityBot/Chatbot.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace CyberSecurityBot
 {
-    internal class Chatbot
+    public static class  Chatbot
     {
+    public static void Start(string name)
+        {
+            UserProfile user = new UserProfile(name);
+            bool running = true;
+
+            while (!running) {
+                
+
+        }
     }
+    
 }
